ebe0ea94ec4b2b2fc998868b2d652a04c72edf66
DanilFedorovsky <danil.fedorovsky@gmail.com>
2022-12-01 23:33:00 +0100

    simulate room

---
+++ simulate_room.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyroomacoustics as pra
+from scipy.io import wavfile
+import os
+from tqdm import tqdm
+
+PATH = "/project/data_asr/CHiME5/data/wsj0-mix2/2speakers/wav8k/min/tr/mix/"
+
+for file in tqdm(os.listdir(PATH)):
+
+    fs, audio = wavfile.read(PATH+file)
+
+    # Create a 4 by 6 metres shoe box room
+    room = pra.ShoeBox([4,6]) # Or AnechoicRoom 
+    room.add_source([2.5, 4.5], signal=audio)
+
+    # Create a linear array with 2 microphones with angle 0 degrees and inter mic distance 10 cm
+    R = pra.linear_2D_array([2, 1.5], 1, 0, 0.1)
+    room.add_microphone_array(pra.Beamformer(R, room.fs))
+
+    # Simulate propagation
+    room.simulate()
+
+    room.mic_array.to_wav(
+        ("/project/data_asr/CHiME5/data/wsj0-mix2/oneChannelRoom/tr/mix/" + str(file)),
+        norm=True,
+        bitdepth=np.int16,
+    )
---
+++ simulate_room_one.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyroomacoustics as pra
+from scipy.io import wavfile
+
+fs, audio = wavfile.read("./outputs/figs/ex.wav")
+
+# Create a 4 by 6 metres shoe box room
+room = pra.ShoeBox([4,6]) # Or AnechoicRoom 
+
+# Add a source somewhere in the room
+room.add_source([2.5, 4.5], signal=audio)
+
+# Create a linear array with 2 microphones
+# with angle 0 degrees and inter mic distance 10 cm
+R = pra.linear_2D_array([2, 1.5], 2, 0, 0.1)
+room.add_microphone_array(pra.Beamformer(R, room.fs))
+
+# Simulate propagation
+room.simulate()
+
+room.mic_array.to_wav(
+    "./outputs/figs/ex_out.wav",
+    norm=True,
+    bitdepth=np.int16,
+)
